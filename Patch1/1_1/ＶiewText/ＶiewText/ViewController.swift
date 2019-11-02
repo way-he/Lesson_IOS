e6ea6eb7572c90ba51bff9b87404656c45934e9f
wayhe <way.he@gigamedia.com.tw>
2019-11-02 20:16:02 +0800

    Patch 1-1

---
+++ Patch1/1_1/ＶiewText/ＶiewText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ＶiewText
+//
+//  Created by 何偉綸 on 2019/11/2.
+//  Copyright © 2019 何偉綸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
